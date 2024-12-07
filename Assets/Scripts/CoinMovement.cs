72f99f32ea33cba2722971cd5c18667371e4bda2
Jacob <jra457@msstate.edu>
2024-12-07 16:35:56 -0600

    CoinMovement set to call PlayerCoins addCoin() function

--- Assets/Scripts/CoinMovement.cs
+++ Assets/Scripts/CoinMovement.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class CoinTracking : MonoBehaviour
 {
 
+    // Coins
     public GameObject coin;
+
+    // Player
     public GameObject player;
+
+    // Speed that the coin moves at
     public float speed;
-    public float destroyThreshold = 0.5f;
+
+    // Distance from player to destroy coin
+    public float dstrThresh = 0.5f;
+    
+    // PlayerCoins script
+    private PlayerCoins playerCoins;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get PlayerCoins script
+        playerCoins = player.GetComponent<PlayerCoins>();
     }
 
     // Update is called once per frame
@@ -24,9 +35,11 @@ public class CoinTracking : MonoBehaviour
         coin.transform.position = Vector3.MoveTowards(coin.transform.position, player.transform.position, speed);
 
         // Destroy coin once it reaches player
-        if (Vector3.Distance(coin.transform.position, player.transform.position) <= destroyThreshold)
+        if (Vector3.Distance(coin.transform.position, player.transform.position) <= dstrThresh)
         {
             Destroy(coin);
+
+            playerCoins.addCoin();
         }
     }
 }
