2c6ff34102f54e2b2f1d0a7a0f76237cf39875f9
Canon <128321409+CanonSb@users.noreply.github.com>
2024-12-08 18:41:20 -0600

    Reorganize asset files

--- Assets/Scripts/Enemy/GoblinSounds.cs
+++ Assets/Scripts/Enemy/GoblinSounds.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class RandomAutoAudioPlayer : MonoBehaviour
+public class GoblinSounds : MonoBehaviour
 {
     // Array to hold your audio clips
     public AudioClip[] audioClips;
