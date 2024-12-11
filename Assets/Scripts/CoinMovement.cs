d1f2837a8aad0f4ad118f38782f5ae2ddfb28913
Jacob <jra457@msstate.edu>
2024-12-10 19:04:46 -0600

    CoinMovement.cs is functional and has debugging statements

--- Assets/Scripts/CoinMovement.cs
+++ Assets/Scripts/CoinMovement.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CoinTracking : MonoBehaviour
 {
 
-    // Coins
+    // Coin
     public GameObject coin;
 
-    // Player
+    // XR Rig
+    public GameObject xrRig;
+
+    // GameController
     public GameObject player;
 
     // Speed that the coin moves at
@@ -18,28 +21,61 @@ public class CoinTracking : MonoBehaviour
     public float dstrThresh = 0.5f;
     
     // PlayerCoins script
-    private PlayerCoins playerCoins;
-
+    public PlayerCoins playerCoins;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get PlayerCoins script
-        playerCoins = player.GetComponent<PlayerCoins>();
+        if (player == null) player = GameObject.FindWithTag("GameController");
+
+        if (player != null)
+        {
+            Debug.Log($"CM GameController EXISTS.");
+            // Get PlayerCoins script
+            playerCoins = player.GetComponent<PlayerCoins>();
+        }
+        else
+        {
+            Debug.LogError("CM GameController NULL!");
+            return;
+        }
+
+        if (playerCoins != null)
+        {
+            Debug.Log($"CM playerCoins EXISTS.");
+        }
+        else
+        {
+            Debug.LogError("CM playerCoins NULL!");
+            return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // Move coin towards player
-        coin.transform.position = Vector3.MoveTowards(coin.transform.position, player.transform.position, speed);
+        coin.transform.position = Vector3.MoveTowards(coin.transform.position, xrRig.transform.position, speed * Time.fixedDeltaTime);
+
+        Debug.Log(Vector3.Distance(coin.transform.position, xrRig.transform.position));
 
         // Destroy coin once it reaches player
-        if (Vector3.Distance(coin.transform.position, player.transform.position) <= dstrThresh)
+        if (Vector3.Distance(coin.transform.position, xrRig.transform.position) <= dstrThresh)
         {
+
+            if (playerCoins != null)
+            {
+                playerCoins.EarnCoins(1);
+                Debug.Log("CM Coin collected! Earned 1 coin.");
+            }
+            else
+            {
+                Debug.LogError("CM playerCoins NULL!");
+            }
+
             Destroy(coin);
 
-            playerCoins.EarnCoins(1);
+            return;
         }
     }
 }
